63ac8e7e01fa6e87dd58aa1f1ba132226b073da9
skylor727 <skylor1997@hotmail.com>
2022-01-21 16:52:55 -0700

    add OAuth functionality

--- server.js
+++ server.js
@@ -3,10 +3,14 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var session = require("express-session");
 var methodOverride = require("method-override");
+var passport = require("passport");
+
 require("dotenv").config();
 
 require("./config/database");
+require("./config/passport");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
@@ -21,8 +25,21 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 app.use(express.static(path.join(__dirname, "public")));
 app.use(methodOverride("_method"));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(function (req, res, next) {
+  res.locals.user = req.user;
+  next();
+});
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);

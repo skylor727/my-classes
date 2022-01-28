0c9320aa6dc0391d2638a1a6e4c070cd40966ad0
skylor727 <skylor1997@hotmail.com>
2022-01-27 17:39:39 -0700

    fix server

---
+++ server.js
@@ -0,0 +1,66 @@
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var session = require("express-session");
+var methodOverride = require("method-override");
+var passport = require("passport");
+
+require("dotenv").config();
+
+require("./config/database");
+require("./config/passport");
+
+var indexRouter = require("./routes/index");
+var classesRouter = require("./routes/classes");
+var reviewsRouter = require("./routes/reviews");
+
+var app = express();
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+app.use(express.static(path.join(__dirname, "public")));
+app.use(methodOverride("_method"));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(function (req, res, next) {
+  res.locals.user = req.user;
+  next();
+});
+const isLoggedIn = require("./config/auth");
+
+app.use("/", indexRouter);
+app.use("/classes", isLoggedIn, classesRouter);
+app.use("/", isLoggedIn, reviewsRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+module.exports = app;
